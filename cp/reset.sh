436f445afe7aa8c235e69a7ed3df7c8ae5c7637c
Rishu Trivedi <rishu.trivedi1997@gmail.com>
2020-06-07 23:04:17 +0530

    Update reset.sh

--- cp/reset.sh
+++ cp/reset.sh
@@ -1,5 +1,5 @@
 mv ~/workspace/cp/host_run.cpp ~/workspace/cp/temp/backup_host.cpp
 cp ~/workspace/cp/temp/host_run.cpp ~/workspace/cp
 
-rm in.txt out.txt
-touch in.txt out.txt
+rm IO/in.txt IO/out.txt
+touch IO/in.txt IO/out.txt
